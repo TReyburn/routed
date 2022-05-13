725a16b41c25097aa6dd0e76c2775d7833f10409
treyburn <travisreyburn@gmail.com>
2022-05-12 21:41:20 -0600

    pylint

--- src/test.py
+++ src/test.py
@@ -1,5 +1,12 @@
-import abc
+"""
+Module docstring
+"""
 
 
-def abcABC(x: int) -> None:
-    x += 1
+def abc_abc(some_value: int) -> None:
+    """
+    Func docstring
+    :param some_value: int
+    :return:
+    """
+    some_value += 1
