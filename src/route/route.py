3a27479666eabd297346aaf210100dbb9fecf578
treyburn <travisreyburn@gmail.com>
2022-05-25 20:52:57 -0600

    slow march towards testable code

--- src/route/route.py
+++ src/route/route.py
@@ -1,4 +1,4 @@
-"""Simple Travelling Salesperson Problem (TSP) between cities."""
+"""Simple Travelling Salesperson Problem (TSP) between cities"""
 from dataclasses import dataclass
 
 from ortools.constraint_solver import pywrapcp, routing_enums_pb2  # type: ignore
@@ -10,7 +10,7 @@ class InputDataModel:
 
     distance_matrix: list[list[int]]
     num_vehicles: int
-    depot: int
+    depot_position: int
 
 
 @dataclass
@@ -21,8 +21,18 @@ class OutputDataModel:
     distance: int
 
 
+@dataclass
+class RouteConstraints:
+    """RouteConstraints"""
+
+    name: str
+    slack: int
+    capacity: int
+    fixed_start: bool = True
+
+
 def create_data_model() -> InputDataModel:
-    """Stores the data for the problem."""
+    """Stores the data for the problem"""
     distance_matrix = [
         [0, 2451, 713, 1018, 1631, 1374, 2408, 213, 2571, 875, 1420, 2145, 1972],
         [2451, 0, 1745, 1524, 831, 1240, 959, 2596, 403, 1589, 1374, 357, 579],
@@ -43,91 +53,91 @@ def create_data_model() -> InputDataModel:
     return InputDataModel(distance_matrix, num_vehicles, depot)
 
 
-def print_solution(
-    manager: pywrapcp.RoutingIndexManager,
-    routing: pywrapcp.RoutingModel,
-    solution: pywrapcp.RoutingModel,
-    num_vehicles: int,
-) -> None:
-    """Prints solution on console."""
-    print(f"Objective: {solution.ObjectiveValue()} miles")
-    for vehicle in range(num_vehicles):
-        index = routing.Start(vehicle)
-        plan_output = f"Route for vehicle {vehicle}:\n"
-        route_distance = 0
-        while not routing.IsEnd(index):
-            plan_output += f" {manager.IndexToNode(index)} ->"
-            previous_index = index
-            index = solution.Value(routing.NextVar(index))
-            route_distance += routing.GetArcCostForVehicle(
-                previous_index, index, vehicle
-            )
-        plan_output += f" {manager.IndexToNode(index)}\n"
-        plan_output += f"Route distance: {route_distance} miles\n"
-        print(plan_output)
+def default_constraints() -> RouteConstraints:
+    """creates default constraints"""
+    return RouteConstraints("Distance", 0, 5500)
 
 
-def parse_output(
-    # manager: pywrapcp.RoutingIndexManager,
-    # routing: pywrapcp.RoutingModel,
-    # solution: pywrapcp.RoutingModel,
-    # num_vehicles: int,
-) -> OutputDataModel:
-    """Parses solution into object"""
-    return OutputDataModel([0], 0)
-
-
-def main() -> None:
-    """Entry point of the program."""
-    # Instantiate the data problem.
-    data = create_data_model()
-
-    # Create the routing index manager.
+def create_route(data: InputDataModel, constraints: RouteConstraints) -> list[OutputDataModel]:
+    """creates the route solution given the input"""
+    # Create the routing index manager
     manager = pywrapcp.RoutingIndexManager(
-        len(data.distance_matrix), data.num_vehicles, data.depot
+        len(data.distance_matrix), data.num_vehicles, data.depot_position
     )
 
-    # Create Routing Model.
+    # Create Routing Model
     routing = pywrapcp.RoutingModel(manager)
 
     def distance_callback(from_index: int, to_index: int) -> int:
-        """Returns the distance between the two nodes."""
-        # Convert from routing variable Index to distance matrix NodeIndex.
+        """Returns the distance between the two nodes"""
+        # Convert from routing variable Index to distance matrix NodeIndex
         from_node: int = manager.IndexToNode(from_index)
         to_node: int = manager.IndexToNode(to_index)
         return data.distance_matrix[from_node][to_node]
 
     transit_callback_index = routing.RegisterTransitCallback(distance_callback)
 
-    # Define cost of each arc.
+    # Define cost of each arc
     routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
 
-    # Add Distance constraint.
-    dimension_name = "Distance"
+    # Add Distance constraint
     routing.AddDimension(
         transit_callback_index,
-        0,  # no slack
-        6000,  # vehicle maximum travel distance
-        True,  # start cumul to zero
-        dimension_name,
+        constraints.slack,  # no slack
+        constraints.capacity,  # vehicle maximum travel distance
+        constraints.fixed_start,  # fixed start cumulative to zero
+        constraints.name,
     )
-    distance_dimension = routing.GetDimensionOrDie(dimension_name)
+    distance_dimension = routing.GetDimensionOrDie(constraints.name)
     distance_dimension.SetGlobalSpanCostCoefficient(100)
 
-    # Setting first solution heuristic.
+    # Setting first solution heuristic
     search_parameters = pywrapcp.DefaultRoutingSearchParameters()
     search_parameters.first_solution_strategy = (
         routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC  # pylint: disable=E1101
     )
 
-    # Solve the problem.
+    # Solve the problem
     solution = routing.SolveWithParameters(search_parameters)
-
-    # Print solution on console.
     if solution is not None:
-        print_solution(manager, routing, solution, data.num_vehicles)
-    else:
-        print("no solution")
+        return parse_solution(manager, routing, solution, data.num_vehicles)
+    raise Exception("err no solution")
+
+
+def parse_solution(
+    manager: pywrapcp.RoutingIndexManager,
+    routing: pywrapcp.RoutingModel,
+    solution: pywrapcp.RoutingModel,
+    num_vehicles: int,
+) -> list[OutputDataModel]:
+    """Parses solution into object"""
+    plan_output: list[OutputDataModel] = []
+    for vehicle in range(num_vehicles):
+        sequence_output: list[int] = []
+        index = routing.Start(vehicle)
+        route_distance = 0
+        while not routing.IsEnd(index):
+            sequence_output.append(manager.IndexToNode(index))
+            previous_index = index
+            index = solution.Value(routing.NextVar(index))
+            route_distance += routing.GetArcCostForVehicle(
+                previous_index, index, vehicle
+            )
+        sequence_output.append(manager.IndexToNode(index))
+        plan_output.append(OutputDataModel(sequence_output, route_distance))
+    return plan_output
+
+
+def main() -> None:
+    """Entry point of the program"""
+    data = create_data_model()
+    constraints = default_constraints()
+
+    try:
+        outcome = create_route(data, constraints)
+        print(outcome)
+    except Exception as err:  # pylint: disable=broad-except
+        print(err)
 
 
 if __name__ == "__main__":
