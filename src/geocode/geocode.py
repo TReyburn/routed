583b2e0984b29fb63402dae42410345ace7b6e59
treyburn <travisreyburn@gmail.com>
2022-11-19 17:04:46 -0700

    Added polyline to subclass check

--- src/geocode/geocode.py
+++ src/geocode/geocode.py
@@ -8,13 +8,16 @@ class GeocoderInterface(metaclass=ABCMeta):
 
     @classmethod
     def __subclasshook__(cls, subclass: Any) -> bool:
-        return callable(subclass.geocode) if hasattr(subclass, "geocode") else False
+        return (
+            callable(subclass.geocode) if hasattr(subclass, "geocode") else False
+        ) and (callable(subclass.polyline) if hasattr(subclass, "polyline") else False)
 
     @abstractmethod
     def geocode(self, addrs: list[str]) -> list[list[int]]:
         """Takes a list of addresses and turns them into a distance matrix"""
         raise NotImplementedError
 
+    @abstractmethod
     def polyline(self, from_a: str, to_b: str) -> str:
         """Creates a polyline representing the route from A to B"""
         raise NotImplementedError
